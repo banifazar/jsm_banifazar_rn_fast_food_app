0802e0ac2ba59b596d0a8b808f33f931883c978b
Bani Faza Rasyadan <bnfzrsydn2@gmail.com>
2025-08-20 20:19:25 +0700

    Routing

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Redirect, Slot } from "expo-router";
+import React from "react";
+
+export default function _Layout() {
+  const isAuthenticated = false; // Replace with your authentication logic
+
+  if (!isAuthenticated) return <Redirect href="/sign-in" />;
+
+  return <Slot />;
+}
