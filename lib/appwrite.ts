b9fde14435781d897246150e92db7c02aef42307
Bani Faza Rasyadan <bnfzrsydn2@gmail.com>
2025-08-22 18:51:46 +0700

    Database Architecture

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -14,7 +14,15 @@ export const appwriteConfig = {
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   projectName: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME,
   databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!,
+  bucketId: process.env.EXPO_PUBLIC_APPWRITE_ASSETS_STORAGE_ID!,
   userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID!,
+  categoriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_CATEGORIES_COLLECTION_ID!,
+  menuCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_COLLECTION_ID!,
+  customizationsCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_CUSTOMIZATIONS_COLLECTION_ID!,
+  menuCustomizationsCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_MENUCUSTOMIZATIONS_COLLECTION_ID!,
 };
 
 export const client = new Client();
