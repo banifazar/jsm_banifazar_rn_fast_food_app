96fd162c1712dc9358d3eb239291882a6d6cab96
Bani Faza Rasyadan <bnfzrsydn2@gmail.com>
2025-08-22 19:08:42 +0700

    Seed Database

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,6 +6,7 @@ import {
   Databases,
   ID,
   Query,
+  Storage,
 } from "react-native-appwrite";
 
 export const appwriteConfig = {
@@ -35,6 +36,7 @@ client
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const avatars = new Avatars(client);
+export const storage = new Storage(client);
 
 export const createUser = async ({
   name,
